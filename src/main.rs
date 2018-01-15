d2f5ac20d1f0d7643ddde53c72376c39121dbb3b
Stephen Weeks <tene@allalone.org>
2018-01-15 14:09:51 -0800

    Sort page index and render individual pages

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,21 @@ use db::models::Page;
 use std::collections::HashMap;
 use rocket_contrib::Template;
 
+#[get("/page/<name>")]
+fn get_single_page(conn: db::Conn, name: String) -> Template {
+    use db::schema::page::dsl::page;
+    let mut ctx = HashMap::new();
+    let thispage = page.find(name)
+        .get_result::<Page>(&*conn)
+        .expect("Failed to fetch page");
+    ctx.insert("page", thispage);
+    Template::render("page", ctx)
+}
 #[get("/pages")]
 fn get_pages(conn: db::Conn) -> Template {
+    use db::schema::page::dsl::page;
     let mut ctx = HashMap::new();
-    let pages = db::schema::page::table
+    let pages = page.order(db::schema::page::name)
         .load::<Page>(&*conn)
         .expect("Failed to fetch pages");
     ctx.insert("pages", pages);
@@ -39,7 +50,7 @@ fn main() {
     //let conn = establish_connection();
     rocket::ignite()
         .manage(db::init_pool())
-        .mount("/", routes![index, get_pages])
+        .mount("/", routes![index, get_pages, get_single_page])
         .attach(Template::fairing())
         .launch();
 }
